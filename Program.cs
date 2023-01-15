600912ea1583b7ebd9e9af19c68610c655f56b3c
Kristan Ruiz <itan_07@hotmail.com>
2023-01-15 02:17:41 -0700

    First Commit- 3rd Version

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace prueba
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("---Animal Race---");
+      Console.WriteLine("There are 3 animals: a horse, dog and cat. Choose their names and then they will compete each other into a race for the victory!");
+
+      Console.WriteLine("What's the horse's name?");
+
+    }
+  }
+
+}
---
+++ animals.cs
@@ -0,0 +1,46 @@
+using System;
+
+class Animals
+{
+  public Animals(string nombre)
+  {
+    nombreAnimal = nombre;
+  }
+
+  public string GetNombre() => nombreAnimal;
+  public string SetNombre(string nuevoNombre) => this.nombreAnimal = nuevoNombre;
+
+  public void Moverse(Animals animalCompetidor)
+  {
+    distanciaRecorrida += this.movimiento;
+  }
+
+  private string nombreAnimal;
+  protected byte movimiento;
+  protected int distanciaRecorrida = 0; // CHECK: ¿Esto sería necesario?
+}
+
+class Gato : Animals
+{
+  public Gato(string nombregato) : base(nombregato)
+  {
+    this.movimiento = 4;
+  }
+
+}
+
+class Caballo : Animals
+{
+  public Caballo(string nombreCaballo) : base(nombreCaballo)
+  {
+    this.movimiento = 7;
+  }
+}
+
+class Perro : Animals
+{
+  public Perro(string nombrePerro) : base(nombrePerro)
+  {
+    this.movimiento = 5;
+  }
+}
